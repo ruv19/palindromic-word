0590c58a48e0fe6d39a9abf0df6621d49520f0e1
Rüveyda Dal <71987230+ruv19@users.noreply.github.com>
2021-04-26 16:28:49 +0300

    Add files via upload

---
+++ palindromik.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main (void)
+{
+	int j=0;
+	char kelime[50],harf[50];
+	printf("\nBir kelime giriniz:");
+	gets(kelime);
+	for(int i=0;i<strlen(kelime);i++)
+	{
+		harf[j]=kelime[i];
+		j++;
+	}
+
+	{
+		printf("Kelime Palindromiktir",strcmp(harf,kelime));
+    }
+	
+
+}
